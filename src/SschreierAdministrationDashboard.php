120ba2d949ce9c9c7da5f53bd8d0e31a7ff24d18
Sebastian Schreier <info@sebastianschreier.de>
2025-02-01 10:15:23 +0100

    php files updated

--- src/SschreierAdministrationDashboard.php
+++ src/SschreierAdministrationDashboard.php
@@ -1,10 +1,16 @@
 <?php
 declare(strict_types=1);
 
+/*
+ * (c) Sebastian Schreier <info@sebastianschreier.de>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Sschreier\AdministrationDashboard;
 
-    use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin;
 
-    class SschreierAdministrationDashboard extends Plugin
-    {
-    }
+class SschreierAdministrationDashboard extends Plugin
+{
+}
